580be7ff423deb3706b9d74a35aafc925089d3cb
fairy042026 <fairy042026@gmail.com>
2020-11-27 21:52:14 +0800

    thirdtest

--- show.js
+++ show.js
@@ -1,10 +1,13 @@
-var leadsRef = firebase.database().ref();引用firebase
+var leadsRef = firebase.database().ref();
 
 //number:貼文編號
 //title:貼文標題
+//text:貼文內文
+
 for(var i in snap.val()) {
     $('#article').append( 
         `<label>編號:${snap.val()[i].number}</label><br> 
         <label>標題:${snap.val()[i].title}</label><br>
+        <label>內文:<br>${snap.val()[i].text}</label><br><br>
     )
 }})
