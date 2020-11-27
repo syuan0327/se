0dd54df3448f941553353b7af759e4eae51818f1
fairy042026 <fairy042026@gmail.com>
2020-11-27 21:34:47 +0800

    secondtest

--- show.js
+++ show.js
@@ -1,6 +1,8 @@
 var leadsRef = firebase.database().ref();引用firebase
 
-for(var i = 1;i < 10; i++) {
+//number:貼文編號
+//title:貼文標題
+for(var i in snap.val()) {
     $('#article').append( 
         `<label>編號:${snap.val()[i].number}</label><br> 
         <label>標題:${snap.val()[i].title}</label><br>
