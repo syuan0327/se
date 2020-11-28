4f58b526e5ce95986df512a90ae339126be78f09
Shihyin Chen <dearsherrychen@gmail.com>
2020-11-29 05:21:03 +0800

    typesetting code

--- new.js
+++ new.js
@@ -1,30 +1,28 @@
 var leadsRef = firebase.database().ref();
 
-
-leadsRef.once("value", snap => { 
-for(var i in snap.val()){
-var str = snap.val()[i].index;
-}
-var index=++str;
-$( "#send" ).click(function() {
-
-    var title = $("#title").val();
-    var content = $("#content").val();
-    
-    if(title==""){alert("標題不可為空")}
-    if(content==""){alert("內文不可為空")}
-    
-    if(title && content)
-    {
-    leadsRef.push({ 
-    "index" : index,
-    "title" : title,
-    "content" : content,
-    })
-    location.href="index.html";
+leadsRef.once("value", snap => {
+    for (var i in snap.val()) {
+        var str = snap.val()[i].index;
     }
-    
+
+    var index = ++str;
+
+    $("#send").click(function () {
+
+        var title = $("#title").val();
+        var content = $("#content").val();
+
+        if (title == "") { alert("Please enter the title.") }  // 標題不可為空
+        if (content == "") { alert("Please enter your text.") }  // 內文不可為空
+
+        if (title && content) {
+            leadsRef.push({
+                "index": index,
+                "title": title,
+                "content": content,
+            })
+            location.href = "index.html";
+        }
+
     })
 })
-
-
