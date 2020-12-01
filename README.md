36e8f2eb33c8a6c48d95458707ce583a6ef22641
ayd0122344 <ycvs210@gmail.com>
2020-12-01 10:31:24 +0800

    functionTestSucceed

--- README.md
+++ README.md
@@ -6,7 +6,4 @@
 <img src="https://github.com/ayd0122344/se/blob/master/work.jpg" style="zoom:50%" width="500px"/>
 
 # se
-軟體工程期中報告
-
-11/25之前交(更改成12/2交)
-
+Realtime Database: https://console.firebase.google.com/u/0/project/se109-d25e7/database/se109-d25e7/data
---
+++ redirect.js
@@ -0,0 +1,16 @@
+// $( "#login" ).click(function() {
+//     location.href="login.html";
+// })
+
+
+// $( "#signup" ).click(function() {
+//     location.href="signup.html";
+// })
+
+$( "#new" ).click(function() {
+    location.href="new.html";
+})
+
+$( "#cancel" ).click(function() {
+    location.href="index.html";
+})
